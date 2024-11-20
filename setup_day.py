dd42232171a86f9731997e84d4e6ed9da700fcf5
Karen Tovar <karen.tovar@datasite.com>
2024-11-20 11:30:26 -0600

    modify paths and created day 1

--- setup_day.py
+++ setup_day.py
@@ -3,13 +3,13 @@ import shutil
 import sys
 
 def create_day_folder(day):
-    folder_name = f"day_{day}"
+    folder_name = f"2024/day_{day}"
     if os.path.exists(folder_name):
-        print(f"Folder {folder_name} already exists!")
+        print(f"Double check the {folder_name} already exists")
         return
 
     shutil.copytree("template", folder_name)
-    print(f"Created folder {folder_name} with solution and test templates.")
+    print(f"Created folder {folder_name} successfully")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
