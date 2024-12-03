bc473a3e26733c3897d4065f2cbac1096f07a4dc
Karen Tovar <karen.tovar@datasite.com>
2024-12-03 15:40:19 -0600

    template update

--- 2024/day_1/solution.py
+++ 2024/day_1/solution.py
@@ -1,5 +1,7 @@
 def solve_part1(data):
+    sum(data)
     pass  # Implement solution for Part 1
 
 def solve_part2(data):
+    sum(data)
     pass  # Implement solution for Part 2
--- template/test_solution.py
+++ template/test_solution.py
@@ -1,9 +1,9 @@
 from solution import solve_part1, solve_part2
 
 def test_solve_part1():
-    data = ["1", "2", "3"]
+    data = [1, 2, 3]
     assert solve_part1(data) == 6  #
 
 def test_solve_part2():
-    data = ["1", "2", "3"]
+    data = [1, 2, 3]
     assert solve_part2(data) == 12 
