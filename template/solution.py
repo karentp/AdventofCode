66016214389e0e5018ce6dd1d92f20762f58a6e1
Karen Tovar <karen.tovar@datasite.com>
2024-12-14 23:47:34 -0600

    day1 completed

--- template/solution.py
+++ template/solution.py
@@ -5,3 +5,10 @@ def solve_part1(data):
 def solve_part2(data):
     sum(data)
     pass  # Implement solution for Part 2
+
+def main():
+    solve_part1()
+    solve_part2()
+
+if __name__ == "__main__":
+    main()
